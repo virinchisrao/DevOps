e3c5c93708a4b4fd1a6f48d03b84819d0997c031
virinchisrao <65751163+virinchisrao@users.noreply.github.com>
2026-03-17 18:29:17 +0530

    pipeline tester

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -18,7 +18,7 @@ public class Application {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started...");
+        log.info("Java app started... in 3...2...1.... GO");
     }
 
     public String getStatus() {
